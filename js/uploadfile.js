dfd5ed0f8bbe61a993e9eee74a21b148abb2affd
Cory Paterson <corypaterson@glaroam2-135-128.wireless.gla.ac.uk>
2018-11-30 10:31:41 +0000

    changed starting zoom level on  map

--- js/uploadfile.js
+++ js/uploadfile.js
@@ -223,7 +223,7 @@ function getRoute(xmlDoc) {
 }
 
 function drawMap() {
-    var mymap = L.map('mapid').setView([55.8718687, -4.2697705], 15)
+    var mymap = L.map('mapid').setView([0, 0], 1)
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
